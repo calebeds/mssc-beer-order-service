bc4436815b75c04e1e6838c7dd88a94d39df6e02
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-09-07 17:22:37 -0300

    add parent pom mssc-brewery pom and delete repetitive code

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl_.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl_.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-03T13:14:42-0300",
+    date = "2023-09-07T17:20:17-0300",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.20.1 (Ubuntu)"
 )
 @Component
--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-04T09:33:50-0300",
+    date = "2023-09-07T17:20:17-0300",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.20.1 (Ubuntu)"
 )
 @Component
