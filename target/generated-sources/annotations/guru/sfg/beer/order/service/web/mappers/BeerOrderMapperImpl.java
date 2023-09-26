11b2e5ebb9b9659d701ff31b426e050de462699b
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-09-26 17:10:26 -0300

    update parent bom

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-13T11:56:09-0300",
+    date = "2023-09-26T17:10:09-0300",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.20.1 (Ubuntu)"
 )
 @Component
