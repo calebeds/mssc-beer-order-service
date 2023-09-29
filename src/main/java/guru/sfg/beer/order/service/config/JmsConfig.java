588f63ad6fca295837fec6f25c40c71977a99b2c
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-09-29 17:30:30 -0300

    handle validate order response

--- src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -9,7 +9,8 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfig {
 
-    public static final String VALIDATE_ORDER_QUEUE = "validate";
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
+    public static final String VALIDATE_ORDER_RESPONSE_QUEUE = "validate-order-response";
 
     @Bean
     public MessageConverter messageConverter() {
